4acf5049360ad094b7f4aa5f819032e081aa3aa1
Bhavana <bhavanaaa.arora@gmail.com>
2023-07-12 21:51:16 +0530

    update

--- views/connection.js
+++ views/connection.js
@@ -1,9 +1,10 @@
-const mysql=require('mysql');
+const mysql=require('mysql2');
 const con=mysql.createConnection({
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     database:process.env.DB_DATABASE,
+    port:process.env.DB_PORT
 })
 con.connect((err,result)=>{
 if (err) throw err;
